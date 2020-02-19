dd223d48655d971b71be2ae20fa43d3e05317d33
Israel Morales <imoralescs@gmail.com>
2020-02-19 12:23:58 -0500

    Update tutorials.js

--- assets/js/tutorials.js
+++ assets/js/tutorials.js
@@ -7,8 +7,9 @@ var accordionItems = document.querySelectorAll(".accordion__header");
 for (const accordion of accordionItems) {
   accordion.addEventListener('click', function(event) {
     event.preventDefault();
-    console.dir(event.target);
+    var accordionHeader = event.target;
     var accordionContent = event.target.nextElementSibling;
+    accordionHeader.classList.toggle("active");
     accordionContent.classList.toggle("active");
   })
 }
